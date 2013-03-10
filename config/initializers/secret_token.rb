4429798bd174b97ded77dbeb2e349619d29289d3
szines <llxgj9597>
2013-03-10 02:50:59 +0000

    generate new rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Acompanysite::Application.config.secret_key_base = 'e8eae76397c1b710a134f8a086c929bfe3fb445033b1936868106e14703643eb5d58e0bccf39ba32a0d8358f3c5312af44ae77b35f65a998df5f5f560e2d82eb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Acompanysite::Application.config.session_store :encrypted_cookie_store, key: '_acompanysite_session'
