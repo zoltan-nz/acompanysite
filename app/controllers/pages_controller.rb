3b441efc94ba8814e7891a99fb7908f009dea1e6
szines <zdebre@gmail.com>
2013-04-30 23:58:40 +0100

    responsive design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,31 @@
 class PagesController < ApplicationController
 
   def home
+     @slideshow_content = [
+         {h1: 'Human detection', h2: 'Real-time consumer statistics<br /> - measurable efficiency'.html_safe, image: 'keocko_slide1.jpg'},
+         {h1: 'Human Sensor', h2: 'has made the dream of marketing experts<br /> come true!'.html_safe, image: 'keocko_slide2.jpg'},
+         {h1: 'Our reports', h2: 'are about Your customers', image: 'keocko_slide3.jpg'},
+         {h1: 'Hourly rate of € 0.09', h2: 'You can decrease or replace the largest part<br /> of your expenses: live labor!'.html_safe, image: 'keocko_slide6.jpg'},
+         ]
+     @slideshow_length = @slideshow_content.length
 
   end
+
+  def solutions
+
+  end
+
+  def technology
+
+  end
+
+  def services
+
+  end
+
+  def contact
+
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Acompanysite::Application.routes.draw do
   root to: 'pages#home'
+  get 'solutions' => 'pages#solutions'
+  get 'technology' => 'pages#technology'
+  get 'services' => 'pages#services'
+  get 'contact' => 'pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
